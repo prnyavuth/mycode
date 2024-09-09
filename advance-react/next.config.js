f4b5901c4b1fd679f02674c388cd747c8a209d83
prnyavuth <apranaysr@gmail.com>
2024-09-09 20:51:15 +0000

    commit learnings

---
+++ advance-react/next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+  reactStrictMode: true, // Enable strict mode for React, helping to identify potential problems in the app
+  swcMinify: true, // Enable SWC-based minification for faster builds and smaller bundle sizes
+}
+
